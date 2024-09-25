dab4de4b9da5753fbc235b4cbefb47dd72eaf940
RdAnjos <rodrigo.oda@hotmail.com>
2024-09-24 22:02:02 -0300

    Testes de Persistencia - AdmServico

--- Test/Domain/Servicos/AdministradorServico.cs
+++ Test/Domain/Servicos/AdministradorServico.cs
@@ -1,6 +1,8 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using MinimalApi.Dominio.Servicos;
 using MinimalApi.Infraestrutura.Db;
 
 namespace MinimalApi.Dominio.Entidades; //buscando do meu projeto API
@@ -11,43 +13,63 @@ public class AdministradorServicoTest
 
     private DbContexto CriarContextoDeTeste()
     {
+
+        //Fizemos isso pois, no momento de execucao esta pegando o appsettings.json da pasta de Prod.
+        var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var path = Path.GetFullPath(Path.Combine(assemblyPath ?? "", "..", "..", "..")); 
+
         var builder = new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
+        .SetBasePath(path ?? Directory.GetCurrentDirectory())
         .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
         .AddEnvironmentVariables();
 
         var configuration = builder.Build();
 
-        //Obter a string de conexao
-        var connectionString = configuration.GetConnectionString("MySql");
-
-        //Configurar o DbcontextOptionsBuilder
-        var options = new DbContextOptionsBuilder<DbContexto>()
-            .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
-            .Options;
-
-        return new DbContexto(options);
+        return new DbContexto(configuration);
     }
 
     [TestMethod]
     public void TestandoSalvarAdministrador()
     {
         // Arrange
+        var context = CriarContextoDeTeste();
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE Administradores");
+
         var adm = new Administrador();
-        adm.Id = 1;
-        adm.Email = "Teste@teste.com";
-        adm.Senha = "Teste@123";
+        adm.Email = "teste@teste.com";
+        adm.Senha = "teste";
         adm.Perfil = "Adm";
 
-        // Act
+        var administradorServico = new AdministradorServico(context);
 
-        var context = CriarContextoDeTeste();
+        // Act
+        administradorServico.Incluir(adm);
 
         // Assert
-        Assert.AreEqual(1,adm.Id);
-        Assert.AreEqual("Teste@teste.com",adm.Email);
-        Assert.AreEqual("Teste@123",adm.Senha);
-        Assert.AreEqual("Adm",adm.Perfil);
-
+        Assert.AreEqual(1, administradorServico.Todos(1).Count());
     }
+
+
+    [TestMethod]
+    public void TestandoBuscaPorId()
+    {
+        // Arrange
+        var context = CriarContextoDeTeste();
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE Administradores");
+
+        var adm = new Administrador();
+        adm.Email = "teste@teste.com";
+        adm.Senha = "teste";
+        adm.Perfil = "Adm";
+
+        var administradorServico = new AdministradorServico(context);
+
+        // Act
+        administradorServico.Incluir(adm);
+        var admDoBanco = administradorServico.BuscaPorId(adm.Id);
+
+        // Assert
+        Assert.AreEqual(1, admDoBanco?.Id);
+    }
+
 }
